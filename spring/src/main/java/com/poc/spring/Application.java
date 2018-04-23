7af4d3d241911a32c23080062946ead530e7b803
Kishore <Kishore@Kishore-PC>
2018-04-23 15:25:09 -0400

    First Commit

---
+++ spring/src/main/java/com/poc/spring/Application.java
@@ -0,0 +1,13 @@
+package com.poc.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("Kishore");
+		SpringApplication.run(Application.class, args);
+	}
+}
